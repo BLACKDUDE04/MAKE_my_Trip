237ad13fd35f01e7b9b62f05c7366854e18fe9e4
BLACKDUDE04 <153643248+BLACKDUDE04@users.noreply.github.com>
2024-04-21 22:15:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './app.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    </>
+    
+  )
+}
+
+export default App
